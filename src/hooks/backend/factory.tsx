3a1475aeed24cd545b6d1bb7c2f1f9634ea3003d
kjeom <jack.jin@kaia.io>
2024-12-29 23:57:21 +0900

    trigger deploy

--- src/hooks/backend/factory.tsx
+++ src/hooks/backend/factory.tsx
@@ -2,13 +2,15 @@ import { ethers } from "ethers";
 import { provider } from "../common/provider.tsx";
 import factoryAbi from "../../contracts/SurveyFactoryV1.sol/SurveyFactoryV1.json";
 
-const factoryV1 = new ethers.Contract(
-  process.env.SURVEY_FACTORY_V1_CONTRACT_ADDRESS || "",
-  factoryAbi.abi,
-  provider
-);
-
 export const getSurveyV1s = async () => {
-  const surveyAddresses = await factoryV1.getSurveys();
-  return surveyAddresses;
+  const surveyFactoryAddr = process.env.SURVEY_FACTORY_V1_CONTRACT_ADDRESS;
+  if (!surveyFactoryAddr) {
+    throw Error("Invalid survey factory address");
+  }
+  const factoryV1 = new ethers.Contract(
+    surveyFactoryAddr,
+    factoryAbi.abi,
+    provider
+  );
+  return await factoryV1.getSurveys();
 };
