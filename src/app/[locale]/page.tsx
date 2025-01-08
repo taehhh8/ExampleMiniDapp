d7e1f68b1e06db1c2eb868f1c2c6ca553b511e9d
kjeom <jack.jin@kaia.io>
2025-01-08 23:40:47 +0900

    redirect surveys page as a default

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Page() {
+  useEffect(() => {
+    window.location.href = "/square/surveys";
+  }, []);
+
   return <div className="flex flex-col gap-5"></div>;
 }
