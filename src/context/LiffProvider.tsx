49e1a83fb71198e80a199dae137bd16c74ba915a
kjeom <jack.jin@kaia.io>
2025-02-17 17:19:39 +0900

    debug probe

--- src/context/LiffProvider.tsx
+++ src/context/LiffProvider.tsx
@@ -126,12 +126,14 @@ export const LiffProvider: React.FC<{ children: React.ReactNode }> = ({
   };
 
   useEffect(() => {
+    alert(window.location.href);
     liff
       .init({
         liffId: process.env.NEXT_PUBLIC_LIFF_ID as string,
       })
       .then(() => {
         console.log("liff initialization is done");
+        alert(window.location.href);
 
         // invited by friends
         const encodedUID = params.encodedUID;
