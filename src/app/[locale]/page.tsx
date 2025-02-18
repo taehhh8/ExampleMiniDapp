eb1336e1926086781d388fcafb828c4fb5677de9
kjeom <jack.jin@kaia.io>
2025-02-18 19:18:16 +0900

    debug probe

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,15 +6,39 @@ import { useSearchParams } from "next/navigation";
 import { useLiff } from "../../context/LiffProvider";
 
 export default function Page() {
-  const { liffObject, loading } = useLiff();
+  const { liffObject, loading, encodedUID } = useLiff();
   const router = useRouter();
+
+  const friends = async (encodedUID: string, idToken: string) => {
+    const result = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/invite/friends`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          encodedUID,
+          idToken,
+        }),
+      }
+    ).then((res) => res.json());
+    return;
+  };
 
   useEffect(() => {
     if (loading) {
       return;
     }
+    if (encodedUID && liffObject && liffObject.isLoggedIn()) {
+      friends(encodedUID as string, liffObject.getAccessToken() as string).then(
+        (res) => {
+          alert(JSON.stringify(res));
+        }
+      );
+    }
     router.push("/square/surveys");
-  }, [loading]);
+  }, [loading, encodedUID]);
 
   return <div className="flex flex-col gap-5"></div>;
 }
